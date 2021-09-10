e31245f19846a2deed2484cdbed99f5c1cbce3e7
Muhammad Haris <harisnilore@yahoo.com>
2021-09-10 16:47:39 +0500

    Add files via upload
    uploading data types, mathematical calculations and even, check exercise solution

---
+++ Basic Data Types/Challenge 2-Check Parity of a Number.py
@@ -0,0 +1,15 @@
+def checkParity(n):
+  if(n%2==0):
+    result=0
+  else:
+    result=1
+  return result
+
+result=checkParity(2)
+print(result)
+result=checkParity(4)
+print(result)
+result=checkParity(7)
+print(result)
+result=checkParity(11)
+print(result)
---
+++ Basic Data Types/Numbers.py
@@ -0,0 +1,5 @@
+a=2
+b=2.5
+print(a + b)
+print ((a + b) * 2)
+print(2 + 2 + 4 - 2/3)
